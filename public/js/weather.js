29b7cb9eb35a6804033c983922a1cf14de617d0b
hehe-tui <1596060544@qq.com>
2022-11-21 23:07:43 +0800

    猫咖

---
+++ public/js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    CONFIG: {
+        "modules": "01234",
+        "background": 5,
+        "tmpColor": "#F47466",
+        "tmpSize": 16,
+        "cityColor": "#F47466",
+        "citySize": 16,
+        "aqiSize": 16,
+        "weatherIconSize": 24,
+        "alertIconSize": 18,
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "zh",
+        "borderRadius": 5,
+        "fixed": "true",
+        "vertical": "middle",
+        "horizontal": "center",
+        "left": "210",
+        "top": 10,
+        "key": "gqDXchD7Kr"
+    }
+}
