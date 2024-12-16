fd83dc3cf7a91ed827c17cfb75faf0822f049046
jiangjiahao <jiang.jh@torayoung.com>
2024-12-16 13:05:29 +0800

    Site updated: 2024-12-16 13:05:28

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FF0000",
+      "tmpSize": "16",
+      "cityColor": "000000",
+      "citySize": "16",
+      "aqiColor": "FF9900",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "12px 10px 10px 10px",
+      "shadow": "0",
+      "language": "zh",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "57bc23d3e22c49249b4e4185106941d6"
+    }
+  }
